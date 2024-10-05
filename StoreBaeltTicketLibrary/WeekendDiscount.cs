3e3f7e6323f4fbddb6c607dbcf1764cf180d98d3
Marco Dybig <mad006@edu.zealand.dk>
2024-10-06 01:10:35 +0200

    Weekend discount

--- StoreBaeltTicketLibrary/WeekendDiscount.cs
+++ StoreBaeltTicketLibrary/WeekendDiscount.cs
@@ -4,6 +4,13 @@ namespace StoreBaeltTicketLibrary
 {
     public static class WeekendDiscount
     {
+        /// <summary>
+        /// Returns the price with a discount
+        /// </summary>
+        /// <param name="car"></param>
+        /// <param name="day"></param>
+        /// <returns>A double that represents the price after weekend discount</returns>
+        /// <exception cref="ArgumentException"></exception>
         public static double PriceWithWeekendDiscount(Vehicle car, string day)
         {
             if (car.GetType() != typeof(Car))
